33dbc47610605a9005a60737b6f8ba93d0a1020c
Aloe2020 <atalextan@gmail.com>
2016-08-04 14:13:51 +1000

    Revert "Revert "Implemented Drag and Drop""
    This reverts commit 2c52ea6df9fec71c2390d84df43468a6df07ab78.

--- BlockCipherTool/src/blockciphertool/BlockCipherTool.java
+++ BlockCipherTool/src/blockciphertool/BlockCipherTool.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -19,13 +20,16 @@ public class BlockCipherTool extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        BorderPane root = new BorderPane();
         
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, 1374, 828);
+        scene.getStylesheets().add(getClass().getResource("/resources/application.css").toExternalForm());
         
         stage.setTitle("CryptoScyph");
         stage.setScene(scene);
         stage.show();
+        
+        root.setCenter(new MainLayout());
     }
 
     /**
---
+++ BlockCipherTool/src/blockciphertool/MainLayout.java
@@ -0,0 +1,204 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blockciphertool;
+
+/**
+ *
+ * @author Aloe2020
+ */import java.io.IOException;
+
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Point2D;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.SplitPane;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author Aloe2020
+ */
+public class MainLayout extends AnchorPane{
+    
+    @FXML SplitPane base_pane;
+    @FXML AnchorPane main_window;
+    @FXML VBox node_list;
+    
+    private DragNode mDragOverIcon = null;
+    
+    private EventHandler<DragEvent> mIconDragOverRoot=null;
+    private EventHandler<DragEvent> mIconDragDropped=null;
+    private EventHandler<DragEvent> mIconDragOverLeftPane=null;
+    
+    public MainLayout() {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(
+            getClass().getResource("/blockciphertool/FXMLDocument.fxml")
+        );
+
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    @FXML
+    private void initialize() {
+        //Add one icon that will be used for the drag-drop process
+        //This is added as a child to the root AnchorPane so it can be 
+        //visible on both sides of the split pane.
+        mDragOverIcon = new DragNode();
+
+        mDragOverIcon.setVisible(false);
+        mDragOverIcon.setOpacity(0.65);
+        getChildren().add(mDragOverIcon); 
+
+        //populate left pane with multiple colored icons for testing
+        for (int i = 0; i < 7; i++) {
+
+            DragNode nde = new DragNode();
+            
+            addDragDetection(nde);
+
+            nde.setType(DragNodeType.values()[i]);
+            node_list.getChildren().add(nde);
+        }
+        
+        buildDragHandlers();
+    }
+    
+    private void addDragDetection(DragNode dragNode) {
+        
+        dragNode.setOnDragDetected(new EventHandler <MouseEvent> () {
+            
+            @Override
+            public void handle(MouseEvent event) {
+                
+                base_pane.setOnDragOver(mIconDragOverRoot);
+                main_window.setOnDragOver(mIconDragOverLeftPane);
+                main_window.setOnDragDropped(mIconDragDropped);
+                
+                DragNode nde = (DragNode) event.getSource();
+                
+                mDragOverIcon.setType(nde.getType());
+                mDragOverIcon.relocateToPoint(new Point2D (event.getSceneX(), event.getSceneY()));
+                
+                ClipboardContent content = new ClipboardContent();
+                DragContainer container = new DragContainer();
+                
+                container.addData ("type", mDragOverIcon.getType().toString());
+                content.put(DragContainer.AddNode, container);
+                
+                mDragOverIcon.startDragAndDrop (TransferMode.ANY).setContent(content);
+                mDragOverIcon.setVisible(true);
+                mDragOverIcon.setMouseTransparent(true);
+                event.consume();
+            }
+        });
+    }
+    
+    private void buildDragHandlers() {
+        mIconDragOverRoot = new EventHandler<DragEvent>() {
+            
+            @Override
+            public void handle (DragEvent event) {
+                Point2D p = main_window.sceneToLocal(event.getSceneX(), event.getSceneY());
+                
+                if (!main_window.boundsInLocalProperty().get().contains(p)) {
+                    mDragOverIcon.relocateToPoint(new Point2D(event.getSceneX(), event.getSceneY()));
+                    return;
+                }
+                
+                event.consume();
+            }
+        };
+        
+        mIconDragOverLeftPane = new EventHandler <DragEvent> () {
+
+            @Override
+            public void handle(DragEvent event) {
+
+                event.acceptTransferModes(TransferMode.ANY);
+
+                mDragOverIcon.relocateToPoint(
+                    new Point2D(event.getSceneX(), event.getSceneY())
+                );
+
+                event.consume();
+            }
+        };
+        
+        mIconDragDropped = new EventHandler <DragEvent> () {
+
+            @Override
+            public void handle(DragEvent event) {
+                
+                DragContainer container = 
+                        (DragContainer) event.getDragboard().getContent(DragContainer.AddNode);
+                
+                container.addData("scene_coords", 
+                        new Point2D(event.getSceneX(), event.getSceneY()));
+
+                ClipboardContent content = new ClipboardContent();
+                content.put(DragContainer.AddNode, container);
+
+                event.getDragboard().setContent(content);
+                event.setDropCompleted(true);
+            }
+        };
+        
+        this.setOnDragDone (new EventHandler <DragEvent> (){
+            
+            @Override
+            public void handle (DragEvent event) {
+
+                main_window.removeEventHandler(DragEvent.DRAG_OVER, mIconDragOverLeftPane);
+                main_window.removeEventHandler(DragEvent.DRAG_DROPPED, mIconDragDropped);
+                base_pane.removeEventHandler(DragEvent.DRAG_OVER, mIconDragOverRoot);
+
+                mDragOverIcon.setVisible(false);
+                
+                DragContainer container = 
+                        (DragContainer) event.getDragboard().getContent(DragContainer.AddNode);
+                
+                if (container != null) {
+                    if (container.getValue("scene_coords") != null) {
+                        DragNode node = new DragNode();
+                        
+                        node.setType(DragNodeType.valueOf(container.getValue("type")));
+                        main_window.getChildren().add(node);
+                        
+                        Point2D cursorPoint = container.getValue("scene_coords");
+                        
+                        node.relocateToPoint(new Point2D(cursorPoint.getX()- 32, cursorPoint.getY() - 32));
+                    }
+                }
+                
+                container = (DragContainer) event.getDragboard().getContent(DragContainer.DragNode);
+                
+                if (container != null) {
+                    if (container.getValue("type") != null)
+                        System.out.println("Moved node " + container.getValue("type"));
+                }
+
+                event.consume();
+            }
+        });
+    }
+    
+}
